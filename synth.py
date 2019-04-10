739ee2f0d4410a7bfddb8247bd585126eafb6eaa
Eoin Roe <eroe001@gold.ac.uk>
2019-04-10 16:51:35 +0100

    set preset 2 volume and worked on bundling js files together

--- synth.py
+++ synth.py
@@ -5,14 +5,19 @@ from flask_assets import Environment, Bundle
 app = Flask(__name__)
 assets = Environment(app)
 
-js = Bundle('js/sketch.js', 'js/keyboard.js', 'js/note.js', 'js/buttons.js',
-            'js/controls.js', 'js/noisegen.js', 'js/oscilloscope.js', 'js/elektron.js',
-            'js/neutron.js', 'js/sh101.js', 'js/highpass.js', 'js/circledial.js', 'js/sines.js',
-            'js/squares.js', 'js/saws.js', 'js/triangles.js', 'js/onepoles.js', 'js/sequencer.js',
-            filters='jsmin', output='gen/packed.js')
+js = Bundle(
+    #Libraries used
+    Bundle('lib/p5.dom.js', 'lib/maxiLib.js', 'lib/maxiExtras.js',),
+    #My code
+    Bundle('js/sketch.js', 'js/keyboard.js', 'js/note.js', 'js/buttons.js',
+        'js/controls.js', 'js/noisegen.js', 'js/oscilloscope.js', 'js/elektron.js',
+        'js/neutron.js', 'js/sh101.js', 'js/highpass.js', 'js/circledial.js', 'js/sines.js',
+        'js/squares.js', 'js/saws.js', 'js/triangles.js', 'js/onepoles.js', 'js/sequencer.js'),
+    filters='jsmin',
+    output='gen/packed.js')
 
-#assets.register('js_all', js)
-
+assets.register('js_all', js)
+#assets.register('js_all', 'lib/p5.min.js', js) 
 
 @app.route('/home')
 def home():
@@ -27,4 +32,4 @@ def playing():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8000)
+    app.run(debug=False,host='0.0.0.0',port=8000)
